f180cd0a70e161dda6010e680967458670b2e642
zynpcelik <108538346+zynpcelik@users.noreply.github.com>
2023-05-03 18:08:49 +0300

    Added react, redux and tailwind

---
+++ src/redux/notes/notesSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const notesStore = createSlice({
+  name: "notes",
+  initialState: {},
+  reducers: {},
+});
+export default notesStore.reducer;
